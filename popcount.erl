1c9dfa0ab29bf92b6b27633aaf01362f6ca22828
alexbowe <bowe.alexander@gmail.com>
2011-04-03 01:46:15 +1100

    add hybrid

--- popcount.erl
+++ popcount.erl
@@ -1,8 +1,8 @@
 -module(popcount).
 -compile({parse_transform,ct_expand}).
--export([popcount/1]).
+-export([popcount32/1]).
 -export([kernighan/1]).
--export([table16/0]).
+-export([hybrid/1]).
 % Check out http://www.valuedlessons.com/2009/01/popcount-in-python-with-benchmarks.html for heaps of these...
 
 % Table expanded at compile time with ct_expand
@@ -21,15 +21,17 @@
             end
         end()
     ) ).
+
 table16() -> ?TABLE(16).
-
-popcount(X) -> erlang:element(X+1, table16()).
-
--define(LOWER_BYTE(X), X band 16#ff).
--define(BYTE(X, I), ?LOWER_BYTE(X bsr I*8)).
+popcount16(V) -> erlang:element(V+1, table16()).
+popcount32(V) -> popcount16( V band 16#ffff ) + popcount16( (V bsr 16) band 16#ffff).
 
 % Works on arbitrary length
 kernighan(V) -> kernighan(0, V).
 kernighan(C, 0) -> C;
 % Clear least significant bit set and count up as we recurse
 kernighan(C, V) -> kernighan(C+1, V band (V-1)).
+
+-define(LIMIT32, ct_expand:term( (2 bsl 31) - 1)).
+hybrid(V) when V =< ?LIMIT32 -> popcount32(V);
+hybrid(V) -> kernighan(V).
