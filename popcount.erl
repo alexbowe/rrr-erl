7c6070b9eda3c5cbabc89bb3dcff320b01b2c093
alexbowe <bowe.alexander@gmail.com>
2011-04-03 16:14:31 +1000

    refactor to make ct_expand easier to read

--- popcount.erl
+++ popcount.erl
@@ -1,29 +1,17 @@
 -module(popcount).
--compile({parse_transform,ct_expand}).
--export([popcount32/1]).
+-compile({parse_transform, ct_expand}).
+-compile({popcount_table}).
+-export([popcount16/1, popcount32/1]).
 -export([kernighan/1]).
 -export([hybrid/1]).
 % Check out http://www.valuedlessons.com/2009/01/popcount-in-python-with-benchmarks.html for heaps of these...
 
-% Table expanded at compile time with ct_expand
--define(TABLE(B), ct_expand:term(
-        fun() ->
-            case B of 0 -> {}; _ ->
-                GenTable = fun(Value, Stop, Table, F) ->
-                    case Value of
-                        Stop -> Table;
-                        _ -> New = (Value band 1) + erlang:element( (Value bsr 1) + 1, Table ),
-                        NewTable = erlang:append_element(Table, New),
-                        F(Value + 1, Stop, NewTable, F)
-                    end
-                end,
-                GenTable(1, 2 bsl (B-1), {0}, GenTable)
-            end
-        end()
-    ) ).
+-define(TABLE(B), ct_expand:term(popcount_table:gen_table(B)) ).
+-define(TABLE16, ct_expand:term(?TABLE(16))).
 
-table16() -> ?TABLE(16).
+table16() -> ?TABLE16.
 popcount16(V) -> erlang:element(V+1, table16()).
+% could make a macro to do this for bigger numbers
 popcount32(V) -> popcount16( V band 16#ffff ) + popcount16( (V bsr 16) band 16#ffff).
 
 % Works on arbitrary length
@@ -35,3 +23,4 @@ kernighan(C, V) -> kernighan(C+1, V band (V-1)).
 -define(LIMIT32, ct_expand:term( (2 bsl 31) - 1)).
 hybrid(V) when V =< ?LIMIT32 -> popcount32(V);
 hybrid(V) -> kernighan(V).
+
---
+++ popcount_table.erl
@@ -0,0 +1,9 @@
+-module(popcount_table).
+-export([gen_table/1]).
+
+gen_table(Bits) -> gen_table(1, 2 bsl (Bits - 1), {0}).
+gen_table(Stop, Stop, Table) -> Table;
+gen_table(Value, Stop, Table) ->
+    New = (Value band 1) + erlang:element((Value bsr 1) + 1, Table),
+    NewTable = erlang:append_element(Table, New),
+    gen_table(Value + 1, Stop, NewTable).
