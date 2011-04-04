e575179f44c683ebfa589e19bb577f2e6179aa59
alexbowe <bowe.alexander@gmail.com>
2011-04-04 23:42:41 +1000

    fast-ish binomial from wikipedia

--- my_math.erl
+++ my_math.erl
@@ -1,7 +1,9 @@
 -module(my_math).
 -export([floor/1, ceil/1]).
 -export([ceillog2/1]).
+-export([binomial/2, binomial0/2]).
 
+% Remove branching?
 % Taken from http://schemecookbook.org/Erlang/NumberRounding
 floor(X) ->
     T = erlang:trunc(X),
@@ -23,3 +25,12 @@ ceil(X) ->
 % Optimize this as per hacker's delight
 -define(INVLOG2, 1.4426950408889634). 
 ceillog2(X) -> ceil(math:log(X) * ?INVLOG2).
+
+%http://www.luschny.de/math/factorial/FastBinomialFunction.html
+% Adapted from scheme example on wikipedia
+% (http://en.wikipedia.org/wiki/Binomial_coefficient)
+% Uses multiplicative formula and integer division
+binomial(N, K) when K < (N - K) -> binomial_iter(N, K, 0, 1);
+binomial(N, K) -> binomial_iter(N, N-K, 0, 1).
+binomial_iter(_, K, I, Prev) when I >= K -> Prev;
+binomial_iter(N, K, I, Prev) -> binomial_iter(N, K, I+1, ((N - I) * Prev) div (I+1)).
