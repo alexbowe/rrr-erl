a2236c01e4deef45ebcd7d48a11689be5262e3e2
alexbowe <bowe.alexander@gmail.com>
2011-04-06 15:40:34 +1000

    interleaved classes and offsets

--- rrr.erl
+++ rrr.erl
@@ -8,32 +8,20 @@
 %select(Position) ->
 %    Position.
 
+% should time this, but it will only affect construction
 class(V) -> popcount:hybrid(V).
 
 % { Classes, Offsets }
--define(EMPTYRRR, {<<>>, <<>>}).
-% Can make a function that builds this function, using ceil(log_2(blocksize)) storage for classes
-make_class_accumulator(Blocksize) ->
-    ClassSize = my_math:ceillog2(Blocksize+1), % remove + 1 coz we don't need to store class 0?
-    fun(Classes, V) -> << Classes/bits, (class(V)):ClassSize >> end.
-
-% Read claude's code - how does he know how many bits in an offset? how is the RRR Table generated and ordered?
-make_offset_accumulator(_) ->
-    fun(Offsets, _) -> << Offsets/bits, 0:1 >> end.
-
-make_accumulator(Blocksize) ->
-    CA = make_class_accumulator(Blocksize),
-    OA = make_offset_accumulator(Blocksize),
-    fun({Classes, Offsets}, V) -> {CA(Classes, V), OA(Offsets, V)} end.
+-define(EMPTY_RRR, <<>>).
 
 %Interleaving may help with caching
-make_interleaved_accumulator(Blocksize) ->
+make_accumulator(Blocksize) ->
     ClassSize = my_math:ceillog2(Blocksize+1),
-    Offset = 0,%fix these
-    OffsetSize = 1, 
-    fun(RRR, V) -> << RRR/bits, (class(V)):ClassSize, Offset:OffsetSize >> end.
+    fun(RRR, V) -> {C, O} = {class(V), 0}, % still not getting offset size, but 'GTable' will help
+        % Should be log2binomial I think?
+        << RRR/bits, C:ClassSize, O:(my_math:binomial(Blocksize, C)) >> end.
 
 % Could count how many bits required in one pass, then allocate that much mem, then do a 2nd pass to fill it in without all the reallocs
 build(Blocksize, Bin) ->
-bin:fold(make_interleaved_accumulator(Blocksize), <<>>, Blocksize, Bin).
+bin:fold(make_accumulator(Blocksize), ?EMPTY_RRR, Blocksize, Bin).
     % Construct list first then convert to binary, or construct binary for full size required and modify it
