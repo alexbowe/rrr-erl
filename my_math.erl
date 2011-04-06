dc525d4a90ece8d32f3ee2879d32b7ea4345831b
alexbowe <bowe.alexander@gmail.com>
2011-04-06 15:40:06 +1000

    improve commenting

--- my_math.erl
+++ my_math.erl
@@ -1,7 +1,9 @@
 -module(my_math).
 -export([floor/1, ceil/1]).
 -export([ceillog2/1]).
--export([binomial/2, binomial0/2]).
+-export([binomial/2]).
+-export([new_bin/2]).
+-export([logfactorial/1]).
 
 % Remove branching?
 % Taken from http://schemecookbook.org/Erlang/NumberRounding
@@ -22,15 +24,25 @@ ceil(X) ->
         _ -> T
     end.
 
-% Optimize this as per hacker's delight
+%TODO: Optimize this as per hacker's delight
 -define(INVLOG2, 1.4426950408889634). 
 ceillog2(X) -> ceil(math:log(X) * ?INVLOG2).
 
-%http://www.luschny.de/math/factorial/FastBinomialFunction.html
+% http://www.luschny.de/math/factorial/FastBinomialFunction.html
 % Adapted from scheme example on wikipedia
 % (http://en.wikipedia.org/wiki/Binomial_coefficient)
-% Uses multiplicative formula and integer division
+% Uses multiplicative formula and integer division, and the symmetry property
 binomial(N, K) when K < (N - K) -> binomial_iter(N, K, 0, 1);
 binomial(N, K) -> binomial_iter(N, N-K, 0, 1).
 binomial_iter(_, K, I, Prev) when I >= K -> Prev;
 binomial_iter(N, K, I, Prev) -> binomial_iter(N, K, I+1, ((N - I) * Prev) div (I+1)).
+%TODO: test this against a table version
+
+%TODO: add table with ct_expand
+-define(PI, 3.141592653589793).
+logfactorial(N) -> X = N+1,
+    (X - 0.5)*math:log(X) - X + (0.5 * math:log(2.0*?PI)) + 1.0/(12.0 * X).
+
+% Approximation for large values of N
+%new_bin(N, K) -> G = logfactorial(N) - logfactorial(K) - logfactorial(N - K),
+%    math:exp(G).
