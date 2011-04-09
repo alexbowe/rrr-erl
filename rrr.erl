fea2902cab231b4a7f65271ca41b9303452ed8b1
alexbowe <bowe.alexander@gmail.com>
2011-04-09 19:05:39 +1000

    fix to use generated data

--- rrr.erl
+++ rrr.erl
@@ -1,5 +1,6 @@
 -module(rrr).
--export([build/2]).
+-include("common.hrl").
+-export([build/1]).
 %-export([rank/1, select/1]).
 
 %rank(Position) ->
@@ -8,20 +9,14 @@
 %select(Position) ->
 %    Position.
 
-% should time this, but it will only affect construction
-class(V) -> popcount:hybrid(V).
-
-% { Classes, Offsets }
--define(EMPTY_RRR, <<>>).
+class(V) -> popcount:popcount16_8(V).
 
 %Interleaving may help with caching
-make_accumulator(Blocksize) ->
-    ClassSize = my_math:ceillog2(Blocksize+1),
-    fun(RRR, V) -> {C, O} = {class(V), 0}, % still not getting offset size, but 'GTable' will help
-        % Should be log2binomial I think?
-        << RRR/bits, C:ClassSize, O:(my_math:binomial(Blocksize, C)) >> end.
+accumulator(RRR, V) -> {C, O} = {class(V), rrr_table:get_offset(V)},
+    << RRR/bits, C:(?CLASSBITS), O:(?CEILLOG2BINOMIAL(C)) >>.
 
-% Could count how many bits required in one pass, then allocate that much mem, then do a 2nd pass to fill it in without all the reallocs
-build(Blocksize, Bin) ->
-bin:fold(make_accumulator(Blocksize), ?EMPTY_RRR, Blocksize, Bin).
-    % Construct list first then convert to binary, or construct binary for full size required and modify it
+-define(EMPTY_RRR, <<>>).
+% Could count how many bits required in one pass, then
+% allocate that much mem, then do a 2nd pass to fill it in without all the reallocs
+build(Bin) ->
+    bin:fold(fun(RRR, V) -> accumulator(RRR, V) end, ?EMPTY_RRR, ?BLOCKSIZE, Bin).
