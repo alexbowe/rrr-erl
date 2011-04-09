6c5f7a7e7219f5420fc45339bf53dffda484026d
alexbowe <bowe.alexander@gmail.com>
2011-04-09 19:05:04 +1000

    make bitshift easier to understand

--- popcount_table.erl
+++ popcount_table.erl
@@ -1,7 +1,7 @@
 -module(popcount_table).
 -export([gen_table/1]).
 
-gen_table(Bits) -> gen_table(1, 2 bsl (Bits - 1), {0}).
+gen_table(Bits) -> gen_table(1, 1 bsl Bits, {0}).
 gen_table(Stop, Stop, Table) -> Table;
 gen_table(Value, Stop, Table) ->
     New = (Value band 1) + erlang:element((Value bsr 1) + 1, Table),
