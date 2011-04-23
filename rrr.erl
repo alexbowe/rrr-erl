eca9de8e855b03418b716b6714674d2d39f56093
alexbowe <bowe.alexander@gmail.com>
2011-04-23 18:00:21 +1000

    initial work on superblocks. May be better to use streams.

--- rrr.erl
+++ rrr.erl
@@ -1,22 +1,38 @@
 -module(rrr).
 -include("common.hrl").
 -export([build/1]).
-%-export([rank/1, select/1]).
+-export([rank/2]).
 
-%rank(Position) ->
-%    Position.
+rank(RRR, Position) ->
+    Position.
 
 %select(Position) ->
 %    Position.
 
-class(V) -> popcount:popcount16_8(V).
+-define(class(V), popcount:popcount16_8(V)).
 
-%Interleaving may help with caching
-accumulator(RRR, V) -> {C, O} = {class(V), rrr_table:get_offset(V)},
-    << RRR/bits, C:(?CLASSBITS), O:(?CEILLOG2BINOMIAL(C)) >>.
+% TODO: USE STREAMS IN ORDER TO PROCESS SUPERBLOCKS IN A SEPARATE FUNCTION
+% one to make the blocks, one to build the superblocks from the blocks, one to combine the two into a binary?
 
--define(EMPTY_RRR, <<>>).
-% Could count how many bits required in one pass, then
-% allocate that much mem, then do a 2nd pass to fill it in without all the reallocs
-build(Bin) ->
-    bin:fold(fun(RRR, V) -> accumulator(RRR, V) end, ?EMPTY_RRR, ?BLOCKSIZE, Bin).
+% TODO: superblocks (need to read up on them)
+get_accumulator(SuperblockSize) ->
+    fun({ {rrr, Superblocks, Blocks}, I} , V) ->
+        {C, O} = {?class(V), rrr_table:get_offset(V)},
+        % Find current superblock from I (use macro), pass temporary counting values along,
+        % only add to binary when a superblock boundary is reached
+        { { rrr,
+            <<Superblocks/bits>>,
+            << Blocks/bits, C:(?CLASSBITS), O:(?CEILLOG2BINOMIAL(C)) >> },
+            I+1 }
+    end.
+
+% FORMAT: { {rrr, SuperBlocks, Blocks}, CurrentBlock }
+% Add temporary block count variable?
+-define(EMPTY_RRR, {{rrr, <<>>, <<>>}, 0}).
+
+-define(SUPERBLOCKSIZE, 32).
+build(Bin) -> build(Bin, ?BLOCKSIZE, ?SUPERBLOCKSIZE).
+build(Bin, BlockSize, SuperblockSize) ->
+    Accumulator = get_accumulator(SuperblockSize),
+    {RRR, _ } = bin:fold(Accumulator, ?EMPTY_RRR, BlockSize, Bin),
+    RRR.
