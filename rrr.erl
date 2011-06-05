f33ed636a6a386bd25765b10c133e9f5a1b98791
alexbowe <bowe.alexander@gmail.com>
2011-06-05 23:58:24 +1000

    update superblocks in accumulator

--- rrr.erl
+++ rrr.erl
@@ -1,38 +1,41 @@
 -module(rrr).
 -include("common.hrl").
--export([build/1]).
+-export([build/1, build/3]).
 -export([rank/2]).
 
-rank(RRR, Position) ->
-    Position.
+rank(RRR, Position) -> 0.
 
 %select(Position) ->
 %    Position.
 
--define(class(V), popcount:popcount16_8(V)).
+-define(SUPERBLOCKSIZE, 32).
+-define(class(V), popcount:popcount16(V)).
+-define(block(V), {?class(V), rrr_table:get_offset(V)}).
+-define(compress_block(C, O), <<C:(?CLASSBITS), O:(?CEILLOG2BINOMIAL(C)) >>).
+-define(append_bits(X, Y), <<X/bits, Y/bits>>).
 
-% TODO: USE STREAMS IN ORDER TO PROCESS SUPERBLOCKS IN A SEPARATE FUNCTION
-% one to make the blocks, one to build the superblocks from the blocks, one to combine the two into a binary?
-
-% TODO: superblocks (need to read up on them)
 get_accumulator(SuperblockSize) ->
-    fun({ {rrr, Superblocks, Blocks}, I} , V) ->
-        {C, O} = {?class(V), rrr_table:get_offset(V)},
-        % Find current superblock from I (use macro), pass temporary counting values along,
-        % only add to binary when a superblock boundary is reached
-        { { rrr,
-            <<Superblocks/bits>>,
-            << Blocks/bits, C:(?CLASSBITS), O:(?CEILLOG2BINOMIAL(C)) >> },
-            I+1 }
+    fun({{rrr, Superblocks, Blocks}, {Count, Offset, I}}, Next) ->
+        {C, O} = ?block(Next),
+        B = ?compress_block(C, O),
+
+        % TODO: Convert to tuple instead of list (or bin?)
+        % Update Superblocks at boundary, but not first
+        case (I rem SuperblockSize == 0) and (I /= 0) of
+            % on a superblock boundary
+            true -> Append = [{Count, Offset}];
+            % not on a superblock boundary:
+            false -> Append = []
+        end,
+
+        {{rrr, Superblocks ++ Append, ?APPEND_BITS(Blocks, B)},
+               {Count + C, Offset + bit_size(B), I + 1}}
     end.
 
-% FORMAT: { {rrr, SuperBlocks, Blocks}, CurrentBlock }
-% Add temporary block count variable?
--define(EMPTY_RRR, {{rrr, <<>>, <<>>}, 0}).
-
--define(SUPERBLOCKSIZE, 32).
+-define(EMPTY_RRR, {rrr, [], <<>>}).
+-define(INIT_RRR, {?EMPTY_RRR, {0, 0, 0}}).
 build(Bin) -> build(Bin, ?BLOCKSIZE, ?SUPERBLOCKSIZE).
-build(Bin, BlockSize, SuperblockSize) ->
+build(Bin, BlockSize, SuperblockSize) when BlockSize =< ?BLOCKSIZE ->
     Accumulator = get_accumulator(SuperblockSize),
-    {RRR, _ } = bin:fold(Accumulator, ?EMPTY_RRR, BlockSize, Bin),
+    {RRR, _} = bin:fold(Accumulator, ?INIT_RRR, BlockSize, Bin),
     RRR.
