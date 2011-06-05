508b714d6b186361c0f6d242b318b56c93d31413
alexbowe <bowe.alexander@gmail.com>
2011-06-05 21:50:47 +1000

    remove erlang generated table

--- popcount_table.erl
+++
@@ -1,9 +0,0 @@
--module(popcount_table).
--export([gen_table/1]).
-
-gen_table(Bits) -> gen_table(1, 1 bsl Bits, {0}).
-gen_table(Stop, Stop, Table) -> Table;
-gen_table(Value, Stop, Table) ->
-    New = (Value band 1) + erlang:element((Value bsr 1) + 1, Table),
-    NewTable = erlang:append_element(Table, New),
-    gen_table(Value + 1, Stop, NewTable).
